f3e8a37aefd96be02fea8d358b4acd5dd950445f
dominik-git <dmnk.kolesar@gmail.com>
2017-03-08 14:45:18 +0100

    Add home and about routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/app/app';
+import About from './components/section1/about';
+import Home from './components/section2/home';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/home" component={Home} />
+    <Route path="/about" component={About} />
+  </Router>
+), document.getElementById('root'));
