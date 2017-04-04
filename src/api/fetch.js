400cf9ba95df4e29a24816c4595beb8f18c76b27
Gary Siladi <gergely.siladi@globallogic.com>
2017-04-04 12:41:19 +0200

    016 - data fetching API fixes

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,29 +1,22 @@
-function constructFetch(type, id) {
-  let urlString = type;
-  if (id) urlString = urlString.concat('/', id);
+// @flow
+function constructFetch(type: string, id: ?number) {
+  const urlString = id ? `${type}/${id}` : type;
   return fetch(`
     https://economist.twivel.io/api/v1/${urlString}/json`, {
       method: 'GET',
     })
-  .then(response => response.json());
-}
-
-export function checkForNumber(id) {
-  if (typeof id !== 'number') {
-    throw Error('ID is not a number');
-  }
+  .then(response => response.json())
+  .catch(err => err);
 }
 
 export function getRoot() {
   return constructFetch('root');
 }
 
-export function getEpisodeByID(id) {
-  checkForNumber(id);
+export function getEpisodeByID(id: number) {
   return constructFetch('episode', id);
 }
 
-export function getSeriesByID(id) {
-  checkForNumber(id);
+export function getSeriesByID(id: number) {
   return constructFetch('series', id);
 }
--- src/api/fetch.test.js
+++ src/api/fetch.test.js
@@ -1,40 +1,37 @@
-import { getRoot, getSeriesByID, getEpisodeByID, checkForNumber } from './fetch';
+import { getRoot, getSeriesByID, getEpisodeByID } from './fetch';
 
-beforeEach(() => {
-  global.fetch = jest.fn().mockImplementation(() => {
-    const p = new Promise((resolve) => {
-      resolve({
-        ok: true,
-        status: 200,
-        json: () => ({
-          id: 55,
-          title: 'Dummy Title',
-        }),
+describe('Checking fetch function: ', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      const p = new Promise((resolve) => {
+        resolve({
+          ok: true,
+          status: 200,
+          json: () => ({
+            id: 55,
+            title: 'Dummy Title',
+          }),
+        });
       });
+      return p;
     });
-    return p;
   });
-});
 
-it('checks getRoot()', async () => {
-  const response = await getRoot();
-  expect(response.id).toBe(55);
-  expect(response.title).toBe('Dummy Title');
-});
+  test('getRoot()', async () => {
+    const response = await getRoot();
+    expect(response.id).toBe(55);
+    expect(response.title).toBe('Dummy Title');
+  });
 
-it('checks getSeriesByID()', async () => {
-  const response = await getSeriesByID(5);
-  expect(response.id).toBe(55);
-  expect(response.title).toBe('Dummy Title');
-});
+  test('getSeriesByID()', async () => {
+    const response = await getSeriesByID(5);
+    expect(response.id).toBe(55);
+    expect(response.title).toBe('Dummy Title');
+  });
 
-it('checks getEpisodeByID()', async () => {
-  const response = await getEpisodeByID(5);
-  expect(response.id).toBe(55);
-  expect(response.title).toBe('Dummy Title');
-});
-
-it('checks checkForNumber()', () => {
-  expect(() => checkForNumber('not a number')).toThrow('ID is not a number');
-  expect(() => checkForNumber(16)).not.toThrow('ID is not a number');
+  test('getEpisodeByID()', async () => {
+    const response = await getEpisodeByID(5);
+    expect(response.id).toBe(55);
+    expect(response.title).toBe('Dummy Title');
+  });
 });
