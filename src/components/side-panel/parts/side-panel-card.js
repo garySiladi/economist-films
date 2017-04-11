c35b01c929aa747e63476b72bb3cae2dc0b370a5
Peter Varholak <peter.varholak@globallogic.com>
2017-04-11 20:30:04 +0200

    Rework side panel into cards

---
+++ src/components/side-panel/parts/side-panel-card.js
@@ -0,0 +1,43 @@
+// @flow
+import React from 'react';
+import { Link } from 'react-router';
+import classnames from 'classnames';
+import './side-panel-card.css';
+
+export type SidePanelCardType = {
+  href: string,
+  icon: string,
+  title: string,
+  isActive?: boolean,
+  onClick?: Function, // TODO: will be removed
+};
+
+const SidePanelCard = ({ href, icon, title, isActive, onClick }: SidePanelCardType) => (
+  <Link
+    onClick={onClick}
+    to={href}
+    className={classnames({
+      'side-panel-card': true,
+      'side-panel-card--active': isActive,
+    })}
+  >
+    <svg
+      width="60"
+      height="60"
+      className={classnames({
+        'side-panel-card__icon': true,
+        [`side-panel-card__icon--${icon}`]: true,
+      })}
+    >
+      <circle cx="30" cy="30" r="29" stroke="silver" strokeWidth="2" fill="white" />
+    </svg>
+    <span className="side-panel-card__title">{title}</span>
+  </Link>
+);
+
+SidePanelCard.defaultProps = {
+  isActive: false,
+  onClick: () => {}, // TODO: will be removed
+};
+
+export default SidePanelCard;
--- src/components/side-panel/side-panel.js
+++ src/components/side-panel/side-panel.js
@@ -1,8 +1,43 @@
 // @flow
 import React from 'react';
+import classnames from 'classnames';
+import SidePanelCard from './parts/side-panel-card';
 import './side-panel.css';
 
+const sidePanelOptions = [
+  {
+    href: '/search',
+    icon: 'search-icon',
+    title: 'Search',
+    isActive: true,
+  },
+  {
+    href: '/home',
+    icon: 'home-icon',
+    title: 'Home',
+  },
+  {
+    href: '/favorites',
+    icon: 'favorites-icon',
+    title: 'Favorites',
+  },
+];
+
 class SidePanel extends React.Component {
+  static createSidePanelCard(cardData, i) {
+    return (
+      <li
+        key={String(i)}
+        className={classnames({
+          'side-panel__option': true,
+          'side-panel__option--active': cardData.isActive || false,
+        })}
+      >
+        <SidePanelCard {...cardData} />
+      </li>
+    );
+  }
+
   constructor() {
     super();
     this.state = {
@@ -21,38 +56,34 @@ class SidePanel extends React.Component {
     }
   };
 
-  handleClick() {
+  handleClick(e: Event) {
+    e.preventDefault();
     this.setState({
       isExpanded: !this.state.isExpanded,
     });
   }
 
   render() {
-    const classname = `side-panel ${this.state.isExpanded ? 'side-panel--expanded' : ''}`;
+    const {
+      id: userId,
+      name: userName,
+      imgUrl: userImg,
+    } = this.props.user;
+    const userObj = {
+      href: `/user/${userId}`,
+      icon: userImg,
+      title: userName,
+      onClick: this.handleClick, // TODO: will be removed
+    };
     return (
-      <div className={classname}>
+      <div
+        className={classnames({
+          'side-panel': true,
+          'side-panel--expanded': this.state.isExpanded,
+        })}
+      >
         <ul className="side-panel__option-list">
-          <li className="side-panel__option">
-            <button
-              className="temp-icon activator"
-              onClick={this.handleClick}
-            >
-              {this.props.user.imgUrl + this.props.user.id}
-            </button>
-            <span className="side-panel__option-title">{this.props.user.name}</span>
-          </li>
-          <li className="side-panel__option side-panel__option--active">
-            <button className="temp-icon" />
-            <span className="side-panel__option-title">Search</span>
-          </li>
-          <li className="side-panel__option">
-            <button className="temp-icon" />
-            <span className="side-panel__option-title">Home</span>
-          </li>
-          <li className="side-panel__option">
-            <button className="temp-icon" />
-            <span className="side-panel__option-title">Favorites</span>
-          </li>
+          {[userObj].concat(sidePanelOptions).map(SidePanel.createSidePanelCard)}
         </ul>
       </div>
     );
