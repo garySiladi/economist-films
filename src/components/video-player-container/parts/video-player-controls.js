2879bdba10e7e8f38d293a1496d3d034a819db2b
dominik-git <dmnk.kolesar@gmail.com>
2017-05-08 20:46:46 +0200

    fastRewind and fastForward change logic +  navigation by keyboard

--- src/components/video-player-container/parts/video-player-controls.js
+++ src/components/video-player-container/parts/video-player-controls.js
@@ -11,41 +11,71 @@ export type VideoPlayerControlsProps = {
   pauseVideo: Function,
   fastForward: Function,
   isVideoPlaying: boolean,
+  isControlSelected: boolean,
   fastRewind: Function,
   progress: number,
 }
-const VideoPlayerControls = ({
-  playVideo,
-  pauseVideo,
-  fastForward,
-  isVideoPlaying,
-  fastRewind,
-  progress,
-  }: VideoPlayerControlsProps) => {
-  const progressBarStyle = {
-    width: `${progress}%`,
-  };
 
-  return (
-    <div className="player-interface">
-      <div className="player-interface__progress-bar-container" >
-        <div className="player-interface__progress-bar" style={progressBarStyle} />
+class VideoPlayerControls extends React.Component {
+  constructor(props: VideoPlayerControlsProps) {
+    super(props);
+    (this: any).handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+  handleKeyPress(event: KeyboardEvent) {
+    if (this.props.isControlSelected) {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.props.fastRewind();
+          break;
+        case 'ArrowRight':
+          this.props.fastForward();
+          break;
+        case ' ':
+          if (this.props.isVideoPlaying === true) {
+            this.props.pauseVideo();
+          } else {
+            this.props.playVideo();
+          }
+          break;
+        default:
+      }
+    }
+  }
+
+  render() {
+    const progressBarStyle = {
+      width: `${this.props.progress}%`,
+    };
+    return (
+      <div className={`player-interface ${this.props.isControlSelected ? 'selected' : ' '}`}>
+        <div className="player-interface__progress-bar-container" >
+          <div className="player-interface__progress-bar" style={progressBarStyle} />
+        </div>
+        <div className="player-interface__navigation-wrapper">
+          <button className="player-interface__navigation-control" onClick={this.props.fastRewind}>
+            <img src={Rewind} alt="right" className="player-interface-navigation-icons" />
+          </button>
+          <button
+            onClick={this.props.isVideoPlaying ? this.props.pauseVideo : this.props.playVideo}
+            className="player-interface__navigation-control"
+          >
+            <img src={this.props.isVideoPlaying ? Pause : Play} alt="Play/Pause" className="player-interface-navigation-icons" />
+          </button>
+          <button
+            className="player-interface__navigation-control"
+            onClick={this.props.fastForward}
+          >
+            <img src={Forward} alt="left" className="player-interface-navigation-icons" />
+          </button>
+        </div>
       </div>
-      <div className="player-interface__navigation-wrapper">
-        <button className="player-interface__navigation-control" onClick={fastRewind}>
-          <img src={Rewind} alt="right" className="player-interface-navigation-icons" />
-        </button>
-        <button onClick={isVideoPlaying ? pauseVideo : playVideo} className="player-interface__navigation-control" >
-          <img src={isVideoPlaying ? Pause : Play} alt="Play/Pause" className="player-interface-navigation-icons" />
-        </button>
-        <button
-          className="player-interface__navigation-control"
-          onClick={fastForward}
-        >
-          <img src={Forward} alt="left" className="player-interface-navigation-icons" />
-        </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default VideoPlayerControls;
--- src/components/video-player-container/parts/video-player.js
+++ src/components/video-player-container/parts/video-player.js
@@ -37,12 +37,16 @@ class VideoPlayer extends React.Component {
 
   constructor(props: VideoPlayerPropsType) {
     super(props);
-    (this: any).moveTime = 10;
+    (this: any).bigMoveTime = 10;
+    (this: any).smallMoveTime = 2;
+    (this: any).videoNode = null;
     (this: any).videoNode = null;
     (this: any).player = null;
     this.state = {
+      moveTime: 10,
       isVideoPlaying: true,
       timeProgress: 0,
+      isControlSelected: true,
     };
     (this: any).handleFastForward = this.handleFastForward.bind(this);
     (this: any).handlePlay = this.handlePlay.bind(this);
@@ -50,19 +54,53 @@ class VideoPlayer extends React.Component {
     (this: any).handleRewind = this.handleRewind.bind(this);
     (this: any).handleEndReached = this.handleEndReached.bind(this);
     (this: any).handleTimeUpdate = this.handleTimeUpdate.bind(this);
+    (this: any).handleNavigationState = this.handleNavigationState.bind(this);
   }
   state: VideoPlayerStateType;
 
   componentDidMount() {
     (this: any).player = videojs((this: any).videoNode, { ...videoJsOptions(this.props.videoUrl) });
+    document.addEventListener('keydown', this.handleNavigationState);
   }
 
   componentWillUnmount() {
     if ((this: any).player) {
       (this: any).player.dispose();
     }
+    document.removeEventListener('keydown', this.handleNavigationState);
   }
+  handleNavigationState(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'ArrowUp':
+        (this: any).setState({
+          isControlSelected: false,
+        });
+        break;
+      case 'ArrowDown':
+        (this: any).setState({
+          isControlSelected: true,
+        });
+        break;
+      case 'Enter':
+        if (!(this: any).state.isControlSelected) {
+          browserHistory.goBack();
+          (this: any).setState({
+            isControlSelected: true,
+          });
+        }
+        break;
+      case 'Backspace':
+        browserHistory.goBack();
+        (this: any).setState({ isControlSelected: true });
+        break;
+      default:
+    }
+  }
+  handleMoveTime() {
+    this.setState({
 
+    });
+  }
   handlePlay() {
     this.setState({
       isVideoPlaying: true,
@@ -76,11 +114,17 @@ class VideoPlayer extends React.Component {
     (this: any).player.pause();
   }
   handleRewind() {
-    (this: any).player.currentTime((this: any).player.currentTime() - (this: any).moveTime);
+    if ((this: any).player.currentTime() > 10) {
+      (this: any).player.currentTime((this: any).player.currentTime() - (this: any).bigMoveTime);
+    } else {
+      (this: any).player.currentTime((this: any).player.currentTime() - (this: any).smallMoveTime);
+    }
   }
   handleFastForward() {
     if ((this: any).player.remainingTime() > 10) {
-      (this: any).player.currentTime((this: any).player.currentTime() + (this: any).moveTime);
+      (this: any).player.currentTime((this: any).player.currentTime() + (this: any).bigMoveTime);
+    } else {
+      (this: any).player.currentTime((this: any).player.currentTime() + (this: any).smallMoveTime);
     }
   }
   handleEndReached() {
@@ -99,7 +143,10 @@ class VideoPlayer extends React.Component {
     return (
       <div className="video-player">
         <div data-vjs-player>
-          <button className="video-player-back-button" onClick={browserHistory.goBack}>
+          <button
+            onClick={browserHistory.goBack}
+            className={`video-player-back-button ${this.state.isControlSelected ? ' ' : 'selected'}`}
+          >
             <img src={Back} alt="Back" className="video-player-back-icons" />
           </button>
           <video
@@ -112,6 +159,7 @@ class VideoPlayer extends React.Component {
             playVideo={this.handlePlay}
             pauseVideo={this.handlePause}
             isVideoPlaying={this.state.isVideoPlaying}
+            isControlSelected={this.state.isControlSelected}
             fastForward={this.handleFastForward}
             fastRewind={this.handleRewind}
             progress={this.state.timeProgress}
