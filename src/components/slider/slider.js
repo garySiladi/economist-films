31be5ed2de56fb8528fee3bd611141fbc67b949a
Marek Talpas <marek.talpas@gmail.com>
2017-06-16 17:25:55 +0200

    implemented slider selection behaviour

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -6,6 +6,36 @@ import SliderStructure from '../../structures/root';
 import './slider.css';
 
 class Slider extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static getSelectorStyle(isSelected, selectedEpisode, data) {
+    const horizontalOffset = -320;
+    const calcOffset = isSelected ? selectedEpisode * horizontalOffset : 0;
+    const sliderMarginWidth = 115;
+    const sliderItemWidth = 320;
+    const leftOffsetWithMargins = Math.abs(calcOffset);
+    const homePageWidth = window.innerWidth - sliderMarginWidth;
+    const itemCount = data.length;
+    const sliderItemsWidth = (itemCount * sliderItemWidth);
+    const homePageWithOffset = homePageWidth + leftOffsetWithMargins;
+    const restOfPage = homePageWithOffset - sliderItemsWidth;
+    let sliderStyle = { transform: `translate(${calcOffset}px, 0px)` };
+    if (calcOffset < 0) {
+      if (restOfPage < sliderItemWidth) {
+        sliderStyle = { transform: `translate(${calcOffset}px, 0px)` };
+        return sliderStyle;
+      }
+      if (restOfPage > sliderItemWidth) {
+        let addition = 0;
+        const homeItemWidth = homePageWidth + sliderItemWidth;
+        const sliderWidth = sliderItemWidth * itemCount;
+        if ((homeItemWidth - sliderWidth) < 0) {
+          addition = Math.ceil(((sliderWidth - homeItemWidth) / sliderItemWidth));
+        }
+        sliderStyle = { transform: `translate(${calcOffset + (((selectedEpisode) - 1 - addition) * sliderItemWidth)}px, 0px)` };
+        return sliderStyle;
+      }
+    }
+    return sliderStyle;
+  }
   static defaultProps = {
     data: [],
     className: 'slider',
@@ -21,20 +51,20 @@ class Slider extends React.Component { // eslint-disable-line react/prefer-state
       className,
       sliderTitle,
       selectedEpisode,
+      data,
     } = this.props;
     const sliderClassName = classnames(
       { [`${className}`]: true },
       { [`${className}--selected`]: isSelected },
       { [`${className}--after-expanded`]: isAfterSelected && isEpisodeExpanded },
     );
-    const horizontalOffset = -320;
-    const calcOffset = isSelected ? selectedEpisode * horizontalOffset : 0;
+    const sliderStyle = Slider.getSelectorStyle(isSelected, selectedEpisode, data);
     return (
       <div className={sliderClassName} >
         <div className="slider-title">{sliderTitle}</div>
         <div
           className="slider-items"
-          style={{ transform: `translate(${calcOffset}px, 0px)` }}
+          style={sliderStyle}
         >
           {this.props.data.map((item, index) =>
             <SliderItem
